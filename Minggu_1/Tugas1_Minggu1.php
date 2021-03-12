b228a608cf67cab7fa9d46dfeb7ea853591ee14d
unknown <65722673+fiiahsan16@users.noreply.github.com>
2021-03-12 06:32:25 -0800

    Create Tugas1_Minggu1.php

---
+++ Minggu_1/Tugas1_Minggu1.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+	Soal
+	Buatlah sebuah kelas bernama ‘buku’, kemudian deklarasikan beberapa properties dari buku tersebut, misalnya: judul buku, pengarang, penerbit,tahun terbit, cetakan
+*/
+
+class buku {
+	var $judul_buku;
+	var $pengarang;
+	var $penerbit;
+	var $tahun_terbit;
+	var $cetakan;
+} ?>
