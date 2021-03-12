e0efec3247f5cfc7a504ad023d7da347b1aa5c31
unknown <65722673+fiiahsan16@users.noreply.github.com>
2021-03-12 07:09:15 -0800

    Create Kegiatan3_Minggu2.php

---
+++ Minggu_2/Kegiatan3_Minggu2.php
@@ -0,0 +1,29 @@
+<?php 
+
+/**
+ Buat file baru dan ketikkan listing program berikut ini :
+ */
+class Car {
+	public $tank;
+
+	public function fill ($float)
+	{
+		$this-> tank += $float;
+		return $this;
+	}
+
+	public function ride($float){
+		$miles = $float;
+		$gallons = $miles/50;
+		$this-> tank -= ($gallons);
+
+		return $this;
+	}
+}
+
+$bmw = new Car();
+
+$tank = $bmw -> fill(10) -> ride(40) -> tank;
+
+echo "The number of gallons left in the tank: ".$tank." gal.";
+?>
