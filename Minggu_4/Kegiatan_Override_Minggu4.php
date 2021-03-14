720e9d6a5acd1bd3cc6eb15c7d4b0888b16087ab
unknown <65722673+fiiahsan16@users.noreply.github.com>
2021-03-14 16:41:18 -0700

    Create Kegiatan_Override_Minggu4.php

---
+++ Minggu_4/Kegiatan_Override_Minggu4.php
@@ -0,0 +1,20 @@
+<?php 
+class Car
+{
+	final public function hello()
+	{
+		return "beep";
+	}
+}
+
+class SportsCar extends Car
+{
+	public function hello()
+	{
+		return "Hallo";
+	}
+}
+
+$sportsCar1 = new SportsCar();
+
+echo $sportsCar1 -> hello(); ?>
