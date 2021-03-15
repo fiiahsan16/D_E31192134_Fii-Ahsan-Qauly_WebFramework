2c03dbe7315af978ff218468f9b7200b08d725d5
unknown <65722673+fiiahsan16@users.noreply.github.com>
2021-03-14 21:31:30 -0700

    Create Latihan3_Protected_Minggu4.php

---
+++ Minggu_4/Latihan3_Protected_Minggu4.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+    Karena property pada kelas parent Tablet di protected, maka untuk mengakses propery parent Tablet pada kelas child Handphone hanya dapat dilakukan secara langsung dari dalam kelas child Handphone,dan tidak dapat diakses
+    di luar kelas yang tidak termasuk child class. 
+ */
+
+class Tablet {
+    protected $merk, $camera, $memory;
+
+    public function setMerk( $merk ) {
+        $this->merk = $merk;
+    }
+
+    public function getMerk() {
+        return $this->merk;
+    }
+
+    public function setCamera( $camera ) {
+        $this->camera = $camera;
+    }
+
+    public function getCamera() {
+        return $this->camera;
+    }
+
+    public function setMemory( $camera ) {
+        $this->camera = $camera;
+    }
+
+    public function getMemory() {
+        return $this->camera;
+    }
+
+    public function getInfoProduk()
+    {
+        $str = "Merk : {$this->merk} <br> Camera : {$this->camera} Gb <br> Memory : {$this->memory} Mp";
+
+        return $str;
+    }
+
+}
+
+class Handphone extends Tablet {
+    public $nama;
+
+    public function __construct( $nama = "nama", $merk = "merk", $camera = 0, $memory = 0 ) {
+
+        $this->nama = $nama;
+        $this->merk = $merk;
+        $this->camera = $camera;
+        $this->memory = $memory;
+    }
+
+    public function getInfoProduk()
+    {
+        $str = "Nama : {$this->nama} <br>" . parent::getInfoProduk();
+        return $str;
+    }
+}
+
+$hp = new Handphone("RedmiA5", "Xiaomi", 8, 3);
+echo $hp->getInfoProduk();
