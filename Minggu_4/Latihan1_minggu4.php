3bb36e94755c7369f578e4d018b2e7bae500bdbe
unknown <65722673+fiiahsan16@users.noreply.github.com>
2021-03-14 19:29:08 -0700

    Create Latihan1_minggu4.php

---
+++ Minggu_4/Latihan1_minggu4.php
@@ -0,0 +1,33 @@
+<?php 
+class mobilLengkap
+{
+	public function nontonTV()
+	{
+		return "TV dihidupkan <br/ >
+				TV mencari chanel<br/ >
+				TV menampilkan gambar";
+	}
+}
+class MobilBMW extends mobilLengkap
+{
+
+}
+class MobilBMWberaksi
+{
+	public function nontonTV()
+	{
+
+	}
+	public function HidupkanMobil()
+	{
+
+	}
+	public function MatikanMobil()
+	{
+
+	}
+	public function ubahGigi()
+	{
+		
+	}
+} ?>
