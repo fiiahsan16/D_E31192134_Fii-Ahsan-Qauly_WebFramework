cede1a70e5d3b3dec770105f7ebc3d2a311b7686
unknown <65722673+fiiahsan16@users.noreply.github.com>
2021-03-17 18:06:21 -0700

    Prak1

---
+++ Minggu_7/CI_Praktikum1/application/views/contact.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact</title>
+</head>
+<body>
+	<h2>Halaman Contact</h2>
+	<p>Selamat datang ini adalah halaman Contact</p>
+</body>
+</html>
---
+++ Minggu_7/CI_Praktikum1/application/views/profil.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profil</title>
+</head>
+<body>
+	<h2>Halaman Profil</h2>
+	<p>Selamat datang ini adalah halaman profil</p>
+</body>
+</html>
