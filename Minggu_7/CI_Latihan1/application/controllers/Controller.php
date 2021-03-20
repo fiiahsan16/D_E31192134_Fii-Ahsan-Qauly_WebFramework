a885ec9d6129a71eab93fcdfa82e663c286e69d5
unknown <65722673+fiiahsan16@users.noreply.github.com>
2021-03-20 04:36:15 -0700

    lat1

---
+++ Minggu_7/CI_Latihan1/application/controllers/Controller.php
@@ -0,0 +1,8 @@
+<?php 
+class Controller extends CI_Controller
+{
+	public function index()
+	{
+		$this->load->view('halo');
+	}
+}  ?>
---
+++ Minggu_7/CI_Latihan1/application/views/halo.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan No 1</title>
+</head>
+<body>
+	<p>Percobaan Nomor 1 Modul 2</p>
+</body>
+</html>
