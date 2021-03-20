4eccd5622807d2c682ebc4f48ef0d0e2d827b195
unknown <65722673+fiiahsan16@users.noreply.github.com>
2021-03-20 05:28:08 -0700

    lat3

---
+++ Minggu_7/CI_Latihan3/ci/application/controllers/Demoview2.php
@@ -0,0 +1,6 @@
+<?php 
+class Demoview2 extends CI_Controller{
+	public function index(){
+		$this->load->view('Demoview');
+	}
+} ?>
---
+++ Minggu_7/CI_Latihan3/ci/application/views/Demoview.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DemoView</title>
+	<style type="text/css">
+		h2{
+			color: #f00;
+			font-size: 20px;
+			border-bottom: dashed 1px #f00;
+		}
+		p{
+			font-style: italic;
+		}
+		<!--<link rel="stylesheet" type="text/css" href="<?php //echo base_url()  ?>">-->
+	</style>
+</head>
+<body>
+	<h2>Demo view dengan css</h2>
+	<p>contoh demo view dengan css</p>
+</body>
+</html>
