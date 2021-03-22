f575a80cc65ff2408d00d131fc768c980417b962
unknown <65722673+fiiahsan16@users.noreply.github.com>
2021-03-22 09:33:27 -0700

    lat4

---
+++ Minggu_7/CI_Latihan4/ci/application/controllers/Demo_controller.php
@@ -0,0 +1,11 @@
+<?php 
+class Demo_controller extends CI_Controller{
+	public function index(){
+		echo "<h2>Demo controller</h2>";
+		echo "<br>Function yang di panggil adalah index";
+	}
+	public function aksi(){
+		echo "<h2>Demo controller</h2>";
+		echo "<br>Function yang dipanggil adalah aksi";
+	}
+} ?>
---
+++ Minggu_7/CI_Latihan4/ci/application/views/demoview.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Demo View Css</title>
+
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/style.css">
+</head>
+<body>
+	<h2>Demo vview dengan CSS</h2>
+	<p>Contoh demo view dengan css</p>
+</body>
+</html>
